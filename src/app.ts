b833322fc5d709aaf63d9ad702d2df28f3ff1ac2
Erik Bamberg <erik.bamberg@web.de>
2026-09-07 18:09:42 +0100

    Building map knowledge (#3)
    * added facility management example
    
    * facility management examples

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,21 @@
 import { management_agent } from './agents';
 import { initOpenTelemetry, withTrace } from './observability/otel';
 import { log } from './logger';
+import { log } from './logger';
 
+
+async function main() {
 
 async function main() {
 
   initOpenTelemetry("knowledgebasereader","0.0.1");
   log.info("App", "starting knowledge base agent...");
+  process.env["KNOWLEDGE_DATABASES"] = "./data/knowledge_bases/facility_management";
+
   const agent= withTrace("management_agent", management_agent);
 
-  const answer = await agent("I want to lease a floor in one of your buildings, what are the available options and what are the prices?");
+  const answer = await agent("which office rooms are nearest to a conference room on the 3rd floor?");
   log.success("App", "answer:");
-  log.json(answer);
 }
 
 main()
